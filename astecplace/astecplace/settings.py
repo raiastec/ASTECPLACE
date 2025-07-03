7994c53a5504d3088c1e7cdda4dda51365dfd93c
Raiani Diniz <raiani.diniz.astec@gmail.com>
2025-07-03 09:39:18 -0400

    feat

--- astecplace/astecplace/settings.py
+++ astecplace/astecplace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0yxt-tz)4zz@+(bf6&_g^5ljs$@a!(v5t5gxkb0fv)rpftph*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

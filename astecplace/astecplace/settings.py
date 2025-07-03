25946c30031d1857f305ac5856132dc72204eb3d
Raiani Diniz <raiani.diniz.astec@gmail.com>
2025-07-03 16:01:35 -0400

    .

--- astecplace/astecplace/settings.py
+++ astecplace/astecplace/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0yxt-tz)4zz@+(bf6&_g^5ljs$@a!(v5t5gxkb0fv)rpftph*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 

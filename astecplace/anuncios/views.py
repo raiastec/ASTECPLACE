c08c41c6e476f89aea7ad4326bdf172abc236aba
Raiani Diniz <raiani.diniz.astec@gmail.com>
2025-07-03 16:25:16 -0400

    config

--- astecplace/anuncios/views.py
+++ astecplace/anuncios/views.py
@@ -74,7 +74,7 @@ def anuncios_por_categorias_nome(request, categoria_id):
         
     }
 
-    template = templates_especiais.get(categoria.nome, 'categoria/pagina_categoria.html')
+    template = templates_especiais.get(categoria.nome, 'categoria/pagina_categorias.html')
 
     return render(request, template, {
         'categoria': categoria,
